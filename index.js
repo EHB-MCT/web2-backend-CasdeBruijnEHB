a505c24043a4a9e0ff36979c57fbac6ad52c01d6
CasdeBruijnEHB <71756854+CasdeBruijnEHB@users.noreply.github.com>
2021-12-07 12:15:53 +0100

    MondoDB opzet - insturen data

---
+++ index.js
@@ -0,0 +1,54 @@
+//Back-end voor eindopdracht.
+/*
+Beginnend met een database op MongoDB voor alle playlists. 
+Database met "Generated Playlist" & "Curated Playlist"
+Dingen die er op moeten
+1. Playlist ID
+2. Title
+3. Description
+4. Foto
+(5. Nummer ID's, even proberen hoe. Ook afhankelijk van Generated vs Curated)
+
+Hoe foto er op zetten? 
+1- Foto's omzetten in hexadeximaal (blobs?).
+2- Of lokaal doorsturen naar express, en het PAD opslaan 
+3- Cloudinary 
+*/
+const {
+    MongoClient
+} = require("mongodb");
+
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = "mongodb+srv://casdb1:admin@curatedplaylists.wf8lj.mongodb.net/Playlists?retryWrites=true&w=majority";
+const client = new MongoClient(url);
+
+// The database to use
+const dbName = "Playlists";
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
+        // Use the collection "people"
+        const col = db.collection("Curatedplaylists");
+        // Construct a document                                                                                                                                                              
+        let playlistdocument = {
+            playlistID: "Test MongoDB",
+            title: "test title",
+            description: "Playlist description",
+            image: "imagelink"
+        }
+        // Insert a single document, wait for promise so we can read it back
+        const p = await col.insertOne(playlistdocument);
+        // Find one document
+        const myDoc = await col.findOne();
+        // Print to the console
+        console.log(myDoc);
+    } catch (err) {
+        console.log(err.stack);
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
