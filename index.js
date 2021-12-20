46ea8b50bcb67cca492dc24187397ef6b9809725
CasdeBruijnEHB <71756854+CasdeBruijnEHB@users.noreply.github.com>
2021-12-20 22:35:16 +0100

    Another try - still not working indexpage

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
 */
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/info.html'));
+    res.sendFile(path.join(__dirname, '/Public/info.html'));
 })
 
 
