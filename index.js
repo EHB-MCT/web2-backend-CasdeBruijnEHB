fd82205311736cc735785a3f27f63100df6be299
CasdeBruijnEHB <71756854+CasdeBruijnEHB@users.noreply.github.com>
2021-12-20 23:47:00 +0100

    procfile added & kleine edit

--- index.js
+++ index.js
@@ -27,12 +27,7 @@ const port = process.env.PORT || 3000;
 const path = require('path');
 
 app.use(express.static('public'));
-/*
-app.get('/', (req, res) => {
-    console.log("test");
-    res.redirect('/info.html')
-})
-*/
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/Public/info.html'));
