572836c75e0ff696f644344868d82a7ddd558cf4
CasdeBruijnEHB <71756854+CasdeBruijnEHB@users.noreply.github.com>
2021-12-20 21:15:57 +0100

    Heroku setup

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ Hoe foto er op zetten?
 //====================
 //Run express
 //====================
-const express = require('express')
+const express = require('express');
+const {
+    json
+} = require('express/lib/response');
+const fs = require("fs/promises");
 const app = express()
 const port = 3000
 
@@ -28,11 +32,13 @@ app.get('/', (req, res) => {
     res.redirect('/info.html')
 })
 
-app.get('/getCuratedPlaylists', (req, res) => {
-    res.send("Curated playlists")
+app.get('/getCuratedPlaylists', async (req, res) => {
+    let val = await getCuratedPlaylist();
+    res.send(val)
 })
-app.get('/getGeneratedPlaylists', (req, res) => {
-    res.send("Generated playlists")
+app.get('/getGeneratedPlaylists', async (req, res) => {
+    let val = await getGeneratedPlaylist();
+    res.send(val)
 })
 
 app.listen(port, () => {
@@ -42,12 +48,11 @@ app.listen(port, () => {
 //=================================
 //MongoDB
 //=================================
-const {
-    MongoClient
-} = require("mongodb");
+
 
 // Replace the following with your Atlas connection string                                                                                                                                        
 const url = "mongodb+srv://casdb1:admin@curatedplaylists.wf8lj.mongodb.net/Playlists?retryWrites=true&w=majority";
+const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(url);
 
 // The database to use
@@ -88,28 +93,40 @@ async function run() {
 //================================
 //Function to get db
 //================================
-function getGeneratedPlaylist() {
-    getPlaylist("GeneratedPlaylists").catch(console.error);
+async function getGeneratedPlaylist() {
+    let val = await getPlaylist("GeneratedPlaylists").catch(console.error);
+    return val;
 }
-//getGeneratedPlaylist();
 
-function getCuratedPlaylist() {
-    getPlaylist("Curatedplaylists").catch(console.error);
+
+async function getCuratedPlaylist() {
+    let val = await getPlaylist("Curatedplaylists").catch(console.error);
+    return val;
 }
 //getCuratedPlaylist();
 
 async function getPlaylist(typePlaylist) {
-    const MongoClient = require('mongodb').MongoClient;
-    const client = new MongoClient(url);
-    //connecting
-    await client.connect();
-    const db = client.db(dbName);
-    //Get right items out of right collection
-    const items = await db.collection(typePlaylist).find().toArray();
+    try {
+        //connecting
+        await client.connect();
+        const db = client.db(dbName);
+        //Get right items out of right collection
+        const items = await db.collection(typePlaylist).find().toArray();
+        items.forEach((item, i) => {
+            //console.log(item.title)
+        })
+        //console.log(arrItems)
 
-    items.forEach((item, i) => {
-        console.log(item.title)
-    })
-    client.close();
+        return items;
 
+    } catch (error) {
+        console.log("Something went wrong!")
+        console.log(error);
+        res.status(500).send({
+            error: 'Something went wrong!',
+            value: error
+        })
+    } finally {
+        await client.close();
+    }
 }
